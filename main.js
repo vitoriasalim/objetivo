3c73928843cb74b4200fd260adcf7fda70b6497c
vitoriasalim <148975005+vitoriasalim@users.noreply.github.com>
2025-04-17 10:05:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+
+const contadores = document.querySelectorAll(".contador");
+
+const tempoObjetivo1 = new Date("2025-11-30T00:00:00");  
+const tempoObjetivo2 = new Date("2025-08-10T00:00:00");  
+const tempoObjetivo3 = new Date("2026-02-01T00:00:00");  
+const tempoObjetivo4 = new Date("2026-02-18T00:00:00");  
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    if (tempoFinal > 0) {
+        return [dias, horas, minutos, segundos];
+    } else {
+        return [0, 0, 0, 0];
+    }
+}
+
+function atualizaCronometro() {
+    for (let i = 0; i < contadores.length; i++) {
+        document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
+    }
+}
+
+function comecaCronometro() {
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
+
+
+let tamanhoFonte = 18; 
+
+
+document.getElementById('aumentar-fonte').addEventListener('click', function () {
+    tamanhoFonte += 2;
+    document.documentElement.style.fontSize = tamanhoFonte + 'px';
+});
+
+
+document.getElementById('diminuir-fonte').addEventListener('click', function () {
+    if (tamanhoFonte > 12) {
+        tamanhoFonte -= 2;
+        document.documentElement.style.fontSize = tamanhoFonte + 'px';
+    }
+});
+
+
+document.getElementById('alternar-contraste').addEventListener('click', function () {
+    document.body.classList.toggle('modo-contraste');
+});
